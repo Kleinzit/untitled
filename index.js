86ece22dd0a6be80fab71e2f3a4b25739bfa97f6
rcklobstr <mkleinzit@gmail.com>
2016-01-02 21:04:09 +0200

    wub wub

--- index.js
+++ index.js
@@ -4,9 +4,17 @@
  */
 var bpm = 120;
 var bps = bpm/60;
+var seq = [200,170,220,170,220,200,170,200];
+var envseq = [1,0.3,0.5];
 
 export function dsp(t) {
-  return 0.2*ExpEnvelope(1,100,0.5,t)*Sqw(110,t) + 0.2*ExpEnvelope(0.2,100,0.5,t)*Sqw(220,t);
+  
+  return Sine(ExpEnvelope(1,0.2,0.2,t),t) * 0.7 + 
+  //Tri(Seq(seq,8,1,t)/4,t) * 0.4 + 
+  //ExpEnvelope(1,100,Seq(envseq,3,1,t),t)*Sqw(Seq(seq,8,1,t),t) * 0.05 + 
+  //ExpEnvelope(1,100,0.5,t)*Sqw(Seq(seq,8,1,t)*2,t) * 0.05 + 
+  //ExpEnvelope(1,100,0.5,t)*Sqw(Seq(seq,8,1,t)*3,t) * 0.05 ;
+  0;
 }
 
 function Sine(freq,t)
@@ -39,3 +47,14 @@ function ExpEnvelope(bar,power,length,t)
   return 1/Math.pow(power,place);
 }
 
+function Seq(seq,len,interval,t)
+{
+  //var measure = t%(len*interval);
+  return seq[Math.floor((t%len)/bps*interval)];
+  //return seq[3];
+}
+
+function Chord(func,mul1,mul2)
+{
+  
+}
